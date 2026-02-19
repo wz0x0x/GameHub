bc26b6c3452567a70b56dc938e831a1430be4271
Lev <libya23hamady@gmail.com>
2026-02-19 17:46:17 +0200

    Delete jbc.loader directory

--- jbc.loader/malloc.js
+++
@@ -1,9 +0,0 @@
-// دالة تخصيص المساحة في الذاكرة
-function malloc(size) {
-    var buffer = new ArrayBuffer(size);
-    return {
-        addr: window.p.read8(window.p.leakptr(buffer).add32(0x10)),
-        buffer: buffer
-    };
-}
-window.malloc = malloc;
